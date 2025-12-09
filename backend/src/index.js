72f649de8acebed0c180a8a1890c8832e2c596e3
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-12-09 15:01:35 +0530

    added vercel file

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite frontend
+    origin: [process.env.FRONTEND_URL || "http://localhost:5173"], // Vite frontend
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
   })
