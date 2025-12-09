c149a58a3e49ff146a72200997656076b6006a50
GOGRAJ DADARWAL <ramkunwarjat00@gmail.com>
2025-12-09 19:54:46 +0530

    fixed

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:3000";
 
 export const axiosInstance = axios.create({
-  baseURL: `${API_BASE}/api`,
+  baseURL: `${API_BASE}`,
   timeout: 60_000,
   headers: { "Content-Type": "application/json" },
 });
